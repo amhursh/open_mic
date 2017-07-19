cea6a526292b5feca6c1479dd655612052ad973b
amhursh <amhursh@gmail.com>
2017-07-19 09:15:07 -0600

    Work on joke class

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,3 +1,10 @@
 class Joke
 
+  attr_accessor :joke_info
+
+  def initialize(joke_info)
+    {id: nil, question: nil, answer: nil} = joke_info
+    @joke_info = joke_info
+  end
+
 end
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -5,4 +5,22 @@ require './lib/joke'
 
 class JokeTest < Minitest::Test
 
+  def test_joke_exists
+    joke = Joke.new
+    assert_instance_of Joke, joke
+  end
+
+  def test_joke_takes_jokes
+    joke = Joke.new({id: 1, question: "knock, knock", answer: "who's there"})
+
+    assert_equal ({id: 1, question: "knock, knock", answer: "who's there"}), joke.joke_info
+  end
+
+  def test_joke_has_id
+    skip
+    joke = Joke.new({id: 1})
+
+    assert_equal 1, joke.id
+  end
+
 end
